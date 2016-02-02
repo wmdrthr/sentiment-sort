05aa9a3d7291a85bca88f250baddeee797483b50
Shantanu Joshi <weemadarthur@yggdrasil.in>
2016-02-02 23:06:30 +0530

    add sort priority

--- python/ssort.py
+++ python/ssort.py
@@ -1,8 +1,20 @@
 #! /usr/bin/python
 # encoding: utf8
 
-def sentiment_sort(ratings, n=0):
-    sorted_ratings = sorted(ratings, key=abs, reverse=True)
+def sentiment_sort(ratings, n=0, sort_priority=None):
+    if sort_priority is None:
+        # reverse because we want descending order
+        sorted_ratings = sorted(ratings, key=abs, reverse=True)
+    else:
+        if sort_priority > 0:
+            sorted_ratings = sorted(ratings, reverse=True)
+        else:
+            sorted_ratings = sorted(ratings)
+
+    if n < 0:
+        # this check needed because negative array indexes are valid in Python
+        raise ValueError("invalid value {} for n".format(n))
+
     if n > 0:
         return sorted_ratings[:n]
     else:
@@ -32,6 +44,20 @@ class TestSentimentSort(unittest.TestCase):
         self.assertEqual([-5, 5], sentiment_sort([-5, 5]))
         self.assertEqual([-5, -4, 3], sentiment_sort([3, -5, -4]))
 
+    def test_first_n(self):
+        ratings = [3, 4, -1, 2, -4, -5]
+        self.assertEqual([-5, 4], sentiment_sort(ratings, 2))
+        self.assertEqual([-5, 4, -4, 3, 2, -1], sentiment_sort(ratings, 10))
+        self.assertEqual([-5, 4, -4, 3, 2, -1], sentiment_sort(ratings, 0))
+        self.assertRaises(ValueError, sentiment_sort, ratings, -1)
+
+    def test_positive_first(self):
+        self.assertEqual([5, -5], sentiment_sort([5, -5], sort_priority=1))
+        self.assertEqual([5, -5], sentiment_sort([-5, 5], sort_priority=1))
+
+    def test_negative_first(self):
+        self.assertEqual([-3, 3], sentiment_sort([3, -3], sort_priority=-1))
+        self.assertEqual([-3, 3], sentiment_sort([-3, 3], sort_priority=-1))
 
 if __name__=='__main__':
     unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(TestSentimentSort))
