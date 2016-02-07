e776f9ab1247cab58535f2c234e304f8599b8fec
Shantanu Joshi <weemadarthur@yggdrasil.in>
2016-02-08 02:47:39 +0530

    fix sort function for sort priority

--- python/ssort.py
+++ python/ssort.py
@@ -1,15 +1,28 @@
 #! /usr/bin/python
 # encoding: utf8
 
+import sys
+import functools
+
 def sentiment_sort(ratings, n=0, sort_priority=None):
+
+    # Python 3 no longer supports a cmp function as an argument to sort
+    # the functools workaround below is form the docs
+    def custom_sort_function(a, b):
+        x, y = abs(a), abs(b)
+
+        if x != y or sort_priority is None:
+            return x - y
+        elif sort_priority > 0:
+            return a - b
+        else:
+            return b - a
+
+    # reverse because we want descending order
     if sort_priority is None:
-        # reverse because we want descending order
         sorted_ratings = sorted(ratings, key=abs, reverse=True)
     else:
-        if sort_priority > 0:
-            sorted_ratings = sorted(ratings, reverse=True)
-        else:
-            sorted_ratings = sorted(ratings)
+        sorted_ratings = sorted(ratings, key=functools.cmp_to_key(custom_sort_function), reverse=True)
 
     if n < 0:
         # this check needed because negative array indexes are valid in Python
@@ -27,9 +40,9 @@ import unittest
 class TestSentimentSort(unittest.TestCase):
 
     def test_basic_cases(self):
-        self.assertEqual([-5, -4, -3, -2, -1], sentiment_sort([-5, -4, -3, -2, -1]))
+        self.assertEqual([-5, -4, -3, -2, -1, 0], sentiment_sort([-5, -4, -3, -2, -1, 0]))
         self.assertEqual([-5, -4, -3, -2, -1], sentiment_sort([-1, -2, -3, -4, -5]))
-        self.assertEqual([5, 4, 3, 2, 1], sentiment_sort([5, 4, 3, 2, 1]))
+        self.assertEqual([5, 4, 3, 2, 1, 0], sentiment_sort([5, 4, 3, 2, 1, 0]))
         self.assertEqual([5, 4, 3, 2, 1], sentiment_sort([1, 2, 3, 4, 5]))
         self.assertEqual([-4, 3], sentiment_sort([3, -4]))
         self.assertEqual([2, -1], sentiment_sort([-1, 2]))
@@ -37,7 +50,7 @@ class TestSentimentSort(unittest.TestCase):
         self.assertEqual([-5, -4], sentiment_sort([-4, -5]))
 
     def test_mixed_ratings(self):
-        self.assertEqual([-5, 4, -4, 3, 2, -1], sentiment_sort([3, 4, -1, 2, -4, -5]))
+        self.assertEqual([-5, 4, -4, 3, 2, -1, 0, 0], sentiment_sort([3, 4, -1, 0, 2, -4, 0, -5]))
 
     def test_stable_sort(self):
         self.assertEqual([5, -5], sentiment_sort([5, -5]))
