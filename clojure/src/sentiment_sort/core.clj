72cb09d0618307210f97f38515a53b14fb2cd89b
Shantanu Joshi <weemadarthur@yggdrasil.in>
2016-02-08 04:10:18 +0530

    start Clojure version

--- clojure/src/sentiment_sort/core.clj
+++ clojure/src/sentiment_sort/core.clj
@@ -1,7 +1,29 @@
 (ns sentiment-sort.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def positive-sort-priority +1)
+(def negative-sort-priority -1)
+(def stable-sort-priority 0)
+
+(defn custom-sort-func
+  [sort-priority]
+  (fn [x y]
+    (let [a (Math/abs x)
+          b (Math/abs y)]
+      (cond
+        (or (not= a b) (zero? sort-priority)) (> a b)
+        (> 0 sort-priority) (> y x)
+        :else (> x y)))))
+
+(defn sentiment-sort
+  "Sort customer ratings from -5 to 5 based on sentiment"
+
+  ([ratings]
+   (sort (custom-sort-func stable-sort-priority) ratings))
+
+  ([ratings sort-priority]
+   (sort (custom-sort-func sort-priority) ratings))
+
+  ([ratings sort-priority N]
+   (take N (sentiment-sort ratings sort-priority))))
+
--- clojure/test/sentiment_sort/core_test.clj
+++ clojure/test/sentiment_sort/core_test.clj
@@ -2,6 +2,38 @@
   (:require [clojure.test :refer :all]
             [sentiment-sort.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest basic-tests
+  (testing "basic test cases"
+    (is (= '(-5 -4 -3 -2 -1 0) (sentiment-sort '(-5 -4 -3 -2 -1 0))))
+    (is (= '(-5 -4 -3 -2 -1)   (sentiment-sort '(-1 -2 -3 -4 -5))))
+    (is (= '(5 4 3 2 1)        (sentiment-sort '(5 4 3 2 1))))
+    (is (= '(5 4 3 2 1)        (sentiment-sort '(1 2 3 4 5))))
+    (is (= '(-4 3)             (sentiment-sort '(3 -4))))
+    (is (= '(2 -1)             (sentiment-sort '(-1 2))))
+    (is (= '(5 4)              (sentiment-sort '(4 5))))
+    (is (= '(-5 -4)            (sentiment-sort '(-4 -5)))))
+
+  )
+
+(deftest mixed_ratings
+  (testing "mixed ratings"
+    (is (= '(-5 4 -4 3 2 -1 0 0) (sentiment-sort '(3 4 -1 0 2 -4 0 -5))))
+    (is (= '() (sentiment-sort '())))))
+
+
+(deftest first-n-ratings
+  (testing "first N ratings"
+    (is (= '(-5 4) (sentiment-sort '(3 4 -1 0 2 -4 0 -5) stable-sort-priority 2)))
+    (is (= '(-5 4 -4 3 2 -1 0 0) (sentiment-sort '(3 4 -1 0 2 -4 0 -5) stable-sort-priority 10)))))
+
+(deftest test-positive-first
+  (testing "positive sort priority"
+    (is (= '(5 -5) (sentiment-sort '(5 -5) positive-sort-priority)))
+    (is (= '(5 -5) (sentiment-sort '(-5 5) positive-sort-priority)))
+    (is (= '(-5 4 -4 3 -3 2 -1) (sentiment-sort '(3 4 -1 2 -4 -5 -3) positive-sort-priority)))))
+
+(deftest test-negative-first
+  (testing "negative sort priority"
+    (is (= '(-3 3) (sentiment-sort '(3 -3) negative-sort-priority)))
+    (is (= '(-3 3) (sentiment-sort '(-3 3) negative-sort-priority)))
+    (is (= '(-5 -4 4 -3 3 2 -1) (sentiment-sort '(3 4 -1 2 -4 -5 -3) negative-sort-priority)))))
